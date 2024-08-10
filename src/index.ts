5030d63ca73d8111c7c33e9746ac167fc86a7fa3
ismsahib <ismayilov.sahib.na@gmail.com>
2024-08-10 17:30:17 +0300

    v1.0

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
+import path from "path";
 import "./aliases";
+import { fetchWithRetries, parseFile } from "./utils";
+import schedule from "node-schedule";
 
-console.log("Hello world!");
+const EVERY_X_HOUR = 4;
+
+const fetchAIRequest = async () => {
+  const idArr = parseFile(path.join(__dirname, "id.txt"));
+  const questions = parseFile(path.join(__dirname, "questions.txt"));
+
+  for (let index = 0; index < idArr.length; index++) {
+    const question = questions[Math.floor(Math.random() * questions.length)];
+
+    console.log(`${idArr[index]} - ${question}`);
+
+    await fetchWithRetries(3, `https://${idArr[index]}.us.gaianet.network/v1/chat/completions`, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: question },
+        ],
+      }),
+    })
+      .then(console.log)
+      .catch(console.log);
+  }
+};
+
+schedule.scheduleJob(`0 */${EVERY_X_HOUR} * * *`, function () {
+  console.log("Run: ", new Date().toLocaleString());
+  fetchAIRequest();
+});
---
+++ src/utils.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+
+export const fetchWithRetries = async (retryCount: number, url: string, rData: RequestInit) => {
+  let count = 0;
+  let lastError = "Over retries";
+  while (count < retryCount) {
+    try {
+      count += 1;
+      const resonse = await fetch(url, rData);
+      if (resonse.ok) {
+        return await resonse.json();
+      } else {
+        throw new Error(resonse.statusText);
+      }
+    } catch (error) {
+      console.log("Retry: ", count);
+      lastError = `Error: ${error}`;
+      console.log(error);
+    }
+  }
+  return Promise.reject(lastError);
+};
+
+export const parseFile = (fileName: string) =>
+  fs
+    .readFileSync(fileName, "utf8")
+    .split("\n")
+    .map((str) => str.trim());
